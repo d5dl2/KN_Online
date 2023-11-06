861822c4b8a9bb010469958d0dbb447fb11ef771
d5dl2 <burak_ahm@hotmail.com>
2023-11-06 11:47:50 +0300

    add error message for WIZ_EFFECT packet if skill not found

--- Client/WarFare/GameProcMain.cpp
+++ Client/WarFare/GameProcMain.cpp
@@ -1121,7 +1121,7 @@ bool CGameProcMain::ProcessPacket(Packet& pkt)
 		else
 		{
 			char szBuffer[128];
-			sprintf(szBuffer, "Unhandled packet. Opcode: 0x%02x", iCmd);
+			sprintf(szBuffer, "WIZ_EFFECT packet, skillID not found: %d", skillId);
 
 			std::string szMessage = szBuffer;
 			m_pUIChatDlg->AddChatMsg(N3_CHAT_NORMAL, szMessage, 0xffff0000);
